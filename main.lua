c9d6b11ffb0aaa45914551db7562d5a4a32c7569
Linus Ramneborg <uberhaxor101@live.com>
2018-06-01 20:40:57 +0000

    Add grid, coordinate system + player selector drawing

--- main.lua
+++ main.lua
@@ -8,12 +8,38 @@ local grid = {
   [3] = {0,0,0}
 }
 
-function love.load()
+-- Player selection variables
+local x = 1
+local y = 1
+local player = 1
+local selectedCoordinate = grid[y][x]
 
-end
+function love.keypressed(key, scancode, isrepeat)
+  -- Coordinate selector
 
-function love.mouse()
+  if key == "up" then
+    if y > 1 then
+      y = y - 1
+    end
+  end
 
+  if key == "down" then
+    if y < 3 then
+      y = y + 1
+    end
+  end
+
+  if key == "right" then
+    if x < 3 then
+      x = x + 1
+    end
+  end
+
+  if key == "left" then
+    if x > 1 then
+      x = x - 1
+    end
+  end
 end
 
 function love.update(dt)
@@ -22,5 +48,36 @@ function love.update(dt)
 end
 
 function love.draw()
-  
+
+
+
+  -- Set color to white for line
+  love.graphics.setColor(255,255,255)
+
+  -- Draw first horizontal line
+  love.graphics.line(1,love.graphics.getHeight()*.33,love.graphics.getWidth(),love.graphics.getHeight()*.33)
+
+  -- Draw second horizontal line
+  love.graphics.line(1,love.graphics.getHeight()*.66,love.graphics.getWidth(),love.graphics.getHeight()*.66)
+
+  -- Draw first vertical line
+  love.graphics.line(love.graphics.getWidth()*.33,1,love.graphics.getWidth()*.33,love.graphics.getHeight())
+
+  -- Draw second vertical line
+  love.graphics.line(love.graphics.getWidth()*.66,1,love.graphics.getWidth()*.66,love.graphics.getHeight())
+
+  -- Select color for selection triangle based on player
+  if player == 1 then
+    love.graphics.setColor(109,162,255)
+  else
+    love.graphics.setColor(255,162,109)
+  end
+
+  -- Draw selection rectangle
+  local xAmp = (x-1)*50
+  local yAmp = (y-1)*50
+  love.graphics.rectangle("line",(x)*xAmp,(y)*yAmp,100,100)
+
+
+  love.graphics.print("x: "..x.."\ny: "..y.."\ng: "..grid[x][y],1,1)
 end
