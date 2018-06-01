cc4ec8ec6aa2c5e0b1f76c72fe1222d41ad94794
Linus Ramneborg <uberhaxor101@live.com>
2018-06-01 20:20:04 +0000

    lots and lots of code

---
+++ main.lua
@@ -0,0 +1,26 @@
+local criss = 1
+local cross = 2
+
+-- Basic XY coordinate system
+local grid = {
+  [1] = {0,0,0},
+  [2] = {0,0,0},
+  [3] = {0,0,0}
+}
+
+function love.load()
+
+end
+
+function love.mouse()
+
+end
+
+function love.update(dt)
+
+
+end
+
+function love.draw()
+  
+end
